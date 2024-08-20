41b869cb0d6f2c352ee7b70af8c11f1919d811ef
Daniel <danieljunior.08@hotmail.com>
2024-08-20 14:12:25 -0300

    conta banco com terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO: Conhecer e importar a classe Scanner
+        // Exibir as mensagens para o usuário
+        // Obter pela classe scanner os valores digitados no terminal
+        // Exibir a mensagem da conta criada
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da agência!");
+        int numeroAgencia = scanner.nextInt();
+
+        String agencia = "067-8";
+
+        scanner.nextLine();
+        System.out.println("Por favor, digite seu nome completo!");
+        String nomeCliente = scanner.nextLine();
+
+        double saldo = 237.48;
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroAgencia + " e seu saldo de " + saldo + " já está disponível para saque.");
+
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# dio-trilha-java-basico
+# dio-desafio-banco-terminal
+
+    🤖Este repositório é um desafio de Sintaxe Básica da trilha JAVA Básico fornecido pela DIO.
+
+        👀Este desafio tem como objetivo simular uma conta bancária através do Terminal/Console.
